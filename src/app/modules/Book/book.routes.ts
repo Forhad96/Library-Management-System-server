85ed978fa14df3e447816e98151e25835ed58006
Forhad96 <forhadairdrop@gmail.com>
2024-11-11 13:15:17 +0600

    feat: add validateRequest middleware for request validation using Zod

--- src/app/modules/Book/book.routes.ts
+++ src/app/modules/Book/book.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { BookControllers } from "./book.controllers";
+import validateRequest from "../../middlewares/validateRequest";
+import { BookValidationSchemas } from "./book.validation";
 
 const router = Router()
 
 
-router.post('/',BookControllers.createBook)
+router.post('/',validateRequest(BookValidationSchemas.zBookSchema) ,BookControllers.createBook)
 
 
 
